d06c23004a499d2c27bc8d657dd57fd21e37729c
anyavasilenok <anyavasilenok02@gmail.com>
2023-02-22 01:49:49 +0300

    project basis

--- course/main/urls.py
+++ course/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from .django_functions import cost_living
 
 urlpatterns = [
     path("", views.start_page, name="start_page"),
-    path("test_page", views.test_function, name="test_page")
+    path("test_page", cost_living.test_function, name="test_page")
 ]
